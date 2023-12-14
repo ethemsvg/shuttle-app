8a6801ac721ae95d53a853798021e5e442a43757
goOdyaga <97707595+goOdyaga@users.noreply.github.com>
2023-12-14 14:21:43 +0300

    input validation has done.

--- lib/Controller/Concretes/Input/InputController.dart
+++ lib/Controller/Concretes/Input/InputController.dart
@@ -3,9 +3,10 @@ import 'package:flutter/cupertino.dart';
 class InputController{
   TextEditingController nameController = TextEditingController();
   TextEditingController surnameController = TextEditingController();
-  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController(text: '05');
   TextEditingController shuttleCodeController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmpasswordController = TextEditingController();
   TextEditingController birthDateController = TextEditingController();
   TextEditingController selectedSchoolController = TextEditingController();
 }
--- lib/Pages/Register/HostessRegisterPage.dart
+++ lib/Pages/Register/HostessRegisterPage.dart
@@ -2,11 +2,21 @@ import 'package:mobile_dev/Controller/Concretes/Hostess/HostessController.dart';
 import 'package:mobile_dev/Controller/Concretes/Input/InputController.dart';
 import 'package:flutter/material.dart';
 
+import '../LogIn/HostessLoginPage.dart';
 
-class HostesRegister extends StatelessWidget {
+class HostesRegister extends StatefulWidget {
+  @override
+  _HostesRegisterState createState() => _HostesRegisterState();
+}
+
+class _HostesRegisterState extends State<HostesRegister> {
+  bool _isObscured = true;
+  bool _isObscured_ = true;
 
   HostessController hostessRegisterController=HostessController();
   InputController inputController=InputController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  //bool _isObscured = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,60 +27,130 @@ class HostesRegister extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Hostess Sign-Up",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Hostess Sign-Up",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: inputController.nameController,
+                      validator:hostessRegisterController.validateName,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter Name",
+                        helperText: "Name must contains only letter",
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: inputController.surnameController,
+                      validator:hostessRegisterController.validateSurname,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter Surname",
+                        helperText: "Surname must contains only letter",
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: inputController.phoneNumberController,
+                      validator:hostessRegisterController.validatePhoneNumber,
+                      keyboardType: TextInputType.datetime,
+                      //initialValue: "05",
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        helperText: "0-(5xx)-xxx-xxxx",
+                       // hintText: "0(5__) - ___ - ____",
+                        labelText: "Enter phone number",
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      obscureText: true,
+                      controller: inputController.shuttleCodeController,
+                      validator:hostessRegisterController.validateShuttleKey,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter Shuttle Code",
+                        helperText: "Enter shuttle code that provided by the school",
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      obscureText: _isObscured,
+                      controller: inputController.passwordController,
+                      validator:hostessRegisterController.validatePassword,
+            
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter Password",
+                        helperText: "Password must be at least 8 characters and include \nan uppercase letter, a lowercase letter, and a digit.",
+                      suffixIcon:IconButton(
+                        icon: Icon(
+                          _isObscured ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            _isObscured = !_isObscured;
+                          });
+                        },
+                      )
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      obscureText: _isObscured_,
+                      controller: inputController.confirmpasswordController,
+                      validator: (value) {
+                        if (value != inputController.passwordController.text) {
+                          return 'Passwords do not match';
+                        }
+                        return null; // Return null if the entered password is valid
+                      },
+                    //keyboardType: TextInputType.,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+
+                        labelText: "Confirm Password",
+                        helperText: "Re-enter your password",
+                          suffixIcon:IconButton(
+                            icon: Icon(
+                              _isObscured_ ? Icons.visibility : Icons.visibility_off,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                _isObscured_ = !_isObscured_;
+                              });
+                            },
+                          )
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async{
+                        if (await hostessRegisterController.register(inputController, _formKey.currentState!))
+                        {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => LogInHostess()),
+                          );
+                        }
+                      },
+                      child: Text("Register"),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: inputController.nameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Enter Name",
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: inputController.surnameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Enter Surname",
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: inputController.phoneNumberController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Enter Phone Number",
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  obscureText: true,
-                  controller: inputController.shuttleCodeController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Enter Shuttle Code",
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () async{
-                    print("Buradasin!");
-                    hostessRegisterController.register(inputController);
-                  },
-                  child: Text("Register"),
-                ),
-              ],
+              ),
             ),
           ),
         ),
